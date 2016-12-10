7fb2eab746259be8b649dd3ec219b15573b0598f
drcrowley <ksv.crowley@gmail.com>
2016-12-11 01:30:40 +0500

    prices

--- app/site/site.prices.js
+++ app/site/site.prices.js
@@ -8,41 +8,90 @@
     Prices.$inject = [];
     
     function Prices() {
+
       var vm = this;
 
       vm.periods = [
         {
           month: 1,
-          salePercent: 0,
+          discountPercent: 0,
           class: 'default'
         },
         {
           month: 3,
-          salePercent: 10,
+          discountPercent: 10,
           class: 'warning'
         },
         {
           month: 6,
-          salePercent: 20,
+          discountPercent: 20,
           class: 'success'
         },
         {
           month: 12,
-          salePercent: 30,
+          discountPercent: 30,
           class: 'danger'
         }
       ];
 
-      vm.price = {
+      vm.tariffs = [
+        {
+          title: 'Free',
+          count: 5,
+          price: 0,
+          class: 'default'
+        },
+        {
+          title: 'Econom',
+          count: 50,
+          price: 490,
+          class: 'warning'
+        },
+        {
+          title: 'Start',
+          count: 150,
+          price: 990,
+          class: 'success'
+        },
+        {
+          title: 'Standart',
+          count: 400,
+          price: 1990,
+          class: 'info'
+        },
+        {
+          title: 'Unlimited',
+          count: 'unlimited',
+          price: 4990,
+          class: 'danger'
+        }
+      ];
+        
+      vm.choosedPeriod = vm.periods[0];
+      vm.choosedTariff = vm.tariffs[0];
 
+      calcDiscountPrice();
+      calcTotalPrice();
+
+      vm.choosePeriod = function(period) {
+        vm.choosedPeriod = period;
+        calcDiscountPrice();
+        calcTotalPrice();
       }
 
-      vm.choosePeriod = function() {
-
+      vm.chooseTariff = function(tariff) {
+        vm.choosedTariff = tariff;
+        calcTotalPrice();
       }
 
-      vm.chooseTariff = function() {
+      function calcDiscountPrice() {
+        vm.tariffs.forEach(function(tarif) {
+          tarif.priceWithDiscount = tarif.price - tarif.price*vm.choosedPeriod.discountPercent/100;
+        });
+      }
 
+      function calcTotalPrice() {
+        vm.totalPrice = vm.choosedTariff.priceWithDiscount * vm.choosedPeriod.month;
       }
 
     }
