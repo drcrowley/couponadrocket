74fd75f85b66185a65a8f46adc6abc7956a013ec
drcrowley <ksv.crowley@gmail.com>
2017-04-03 23:38:50 +0500

    fixes

--- src/app/sent/sent.js
+++ src/app/sent/sent.js
@@ -5,9 +5,9 @@
     .module('app.sent')
     .controller('Sent', Sent);
 
-    Sent.$inject = ['$rootScope', '$routeParams', 'translations', 'dataservice'];
+    Sent.$inject = ['$rootScope', '$routeParams', 'translations', 'dataservice', 'auth', 'config'];
     
-    function Sent($rootScope, $routeParams, translations, dataservice) {
+    function Sent($rootScope, $routeParams, translations, dataservice, auth, config) {
 
       $rootScope.title = translations['C_HEAD_SITES'] + ' - ' + translations['C_HEAD_SENT_COUPONS'];
 
@@ -33,13 +33,15 @@
               dataservice.setCurrentSite(site.id);
               $location.path('/site/'+ site.id + '/coupon');
             }
+
+            vm.coupon = site;
           } else {
             dataservice.removeCurrentSite();
             $location.path('/site/new/coupon');
           }
 
+          vm.exportUrl = config.apiUrl + '/manage/destibutedPromoCodes/' + vm.coupon.id + '/' + auth.get();
         });
       }
-
     }
 })();
